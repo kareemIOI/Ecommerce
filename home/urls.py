a5f32a36850a8e824088de362edce4f99fc520fe
Kareem <kareemiisafar@gmail.com>
2023-01-03 23:39:45 +0300

    mappingurls
    Signed-off-by: Kareem <kareemiisafar@gmail.com>

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-]
+    path('productDetails', views.product_details, name = 'productDetails'),
+    path('checkout', views.checkout, name = 'checkout'),
+    path('cart', views.cart, name = 'cart'),
+    path('login', views.login, name = 'login'),
+
+]	
--- home/views.py
+++ home/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def product_details(request):
+    return render(request, 'product-details.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def login(request):
+    return render(request, 'login.html')
