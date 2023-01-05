a0dda13524ba6ce77da2dcd19c65c9e7feeb9125
Kareem <kareemiisafar@gmail.com>
2023-01-06 01:11:55 +0300

    backend mapping1.1
    Signed-off-by: Kareem <kareemiisafar@gmail.com>

--- home/urls.py
+++ home/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
     path('checkout', views.checkout, name = 'checkout'),
     path('cart', views.cart, name = 'cart'),
     path('login', views.login, name = 'login'),
-
+    path('account', views.account, name = 'account'),
+    path('wishlist', views.wishlist, name = 'wishlist'),
+    path('checkout', views.checkout, name = 'checkout'),
+    path('bloglist', views.bloglist, name = 'bloglist'),
+    path('blogsingle', views.blogsingle, name = 'blogsingle'),
+    path('404', views.error_404, name = '404'),
+    path('contact', views.contact, name = 'contact'),
 ]	
--- home/views.py
+++ home/views.py
@@ -19,3 +19,32 @@ def cart(request):
 
 def login(request):
     return render(request, 'login.html')
+
+
+def account(request):
+    return render(request, 'account.html')
+
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def bloglist(request):
+    return render(request, 'bloglist.html')
+
+
+def blogsingle(request):
+    return render(request, 'blogsingle.html')
+
+
+
+def error_404(request):
+    return render(request, '404.html')
+
+
+def contact(request):
+    return render(request, 'contact-us.html')
