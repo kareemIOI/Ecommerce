adc26f641478f9a04396b295af573cab4615e1ca
Kareem <kareemiisafar@gmail.com>
2023-01-07 17:36:46 +0300

    url mapping bug fixes
    Signed-off-by: Kareem <kareemiisafar@gmail.com>

--- home/urls.py
+++ home/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('login', views.login, name = 'login'),
     path('Login', views.Login, name = 'Login'),
     path('logout', views.logout, name = 'logout'),
+    path('products', views.products, name = 'products'),
 ]
